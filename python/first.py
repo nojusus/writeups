4280be7cfac1b41eb0713005990162740f6bf8b7
nojusus <124867797+nojusus@users.noreply.github.com>
2026-04-16 10:42:17 +0300

    Update first.py

--- python/first.py
+++ python/first.py
@@ -1,7 +1,7 @@
 with open("first.txt", "r") as f:
     read = f.read()
 
-# Extract specific line to get the "flag"
+# Extract specific line to get the "flag".
 flagLine = ""
 for line in read.splitlines():
     if "flag" in line:
@@ -9,12 +9,12 @@ for line in read.splitlines():
         print(flagLine)
         break
 
-# Count occurences of the letter "A"
+# Count occurences of the letter "A".
 count = 0
 for letter in read:
     if letter.upper() == "A":
         count += 1
 print(f"\nThere are {count} A letters.")
 
-# Reformat the found line into uppercase
+# Reformat the found line into uppercase.
 print(f"\n{flagLine.upper()}")
